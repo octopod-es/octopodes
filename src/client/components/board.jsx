526ae9e328d5d12dc96fb66af65049657576cdc1
Sejan Miah <sejan.miah@gmail.com>
2019-12-05 10:28:09 -0500

    Initial test commit

--- src/client/components/board.jsx
+++ src/client/components/board.jsx
@@ -21,7 +21,7 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+//TESTING REPO
     };
   }
 
--- src/client/components/column.jsx
+++ src/client/components/column.jsx
@@ -13,6 +13,8 @@ const mapStateToProps = (state) => ({
   offer: state.jobCards.offer,
   accepted: state.jobCards.accepted,
 });
+
+//GIVE CARD ABILITY TO SUBMIT ITSELF
 const mapDispatchToProps = (dispatch) => ({
   dispatchSubmitInfo: (company, role, link) => dispatch(actions.submitInfoActionCreator(company, role, link)),
 });
@@ -27,7 +29,9 @@ class Column extends Component {
 
   render() {
     const relevantCards = [];
+    //ID IS COLUMN NAME, COLUMN NAME WITHIN PROPS IS ASSOCIATED WITH THE ARRAY OF CARD OBJECTS
     const arrayInState = this.props[this.props.id];
+    //ARRAYINSTATE = ARRAY OF CARDS
     for (let i = 0; i < arrayInState.length; i += 1) {
       relevantCards.push(<Card jobObject={arrayInState[i]} inArray key={`arrayCard${i}`} />);
     }
